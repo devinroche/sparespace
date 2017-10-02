426c28970267144c6919b7447c2c89e26ecf9bb2
George Kunthara <gkunthara1@gmail.com>
2017-10-02 13:14:02 -0700

    Test
    Init commit

---
+++ src/components/Login.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
+
+export class Login extends Component {
+    render() {
+        return (
+            <Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#">SpareSpace</a>
+                    </Navbar.Brand>
+                </Navbar.Header>
+                <Nav className={"pull-right"}>
+                    <NavItem eventKey={1} href="#">
+                        Link
+                    </NavItem>
+                    <NavItem eventKey={2} href="#">
+                        Link
+                    </NavItem>
+                </Nav>
+            </Navbar>
+        );
+    }
+}
