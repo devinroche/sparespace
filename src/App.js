e2fcecf5adbcc8cb1ad9df4438a80d9ec3cb97fd
George Kunthara <gkunthara1@gmail.com>
2017-10-02 15:20:45 -0700

    Login/Routes
    Add Login page, add react-router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import AppNavBar from "./components/Nav-bar";
-import SearchBar from "./components/SearchBar";
+import {BrowserRouter as Router} from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <AppNavBar />
-        </header>{" "}
-        <p className="App-intro">
-          <SearchBar />
-        </p>{" "}
-      </div>
+
+        //
+        <Router>
+          <div className="App">
+            <header className="App-header">
+              <AppNavBar />
+            </header>{" "}
+          </div>
+        </Router>
     );
   }
 }
