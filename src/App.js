47ffece9c267c4a652552edebf6c08bf963e6f6d
David Hanany <davidhanany@Davids-MacBook-Pro.local>
2017-10-02 11:59:50 -0700

    homepage stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import {Header} from "./Header"
-import {WhatIsSS} from "./WhatIsSS";
-
-
+import React, { Component } from "react";
+import AppNavBar from "./components/Nav-bar";
+import SearchBar from "./components/SearchBar";
 
 class App extends Component {
   render() {
-
     return (
       <div className="App">
-          <Header/>
-          <WhatIsSS/>
+        <header className="App-header">
+          <AppNavBar />
+        </header>{" "}
+        <p className="App-intro">
+          <SearchBar />
+        </p>{" "}
       </div>
     );
   }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import {
+  Row,
+  Col,
+  FormGroup,
+  ControlLabel,
+  FormControl
+} from "react-bootstrap";
+
+export default class AppNavbar extends Component {
+  render() {
+    return (
+      <Row style={{ marginTop: "25%" }}>
+        <Col sm={8} smdOffset={2} className={"text-center col-sm-offset-2"}>
+          <FormGroup controlId="formBasicText">
+            <ControlLabel style={{ fontSize: "22px", marginBottom: "20px" }}>
+              Save up to 50% in storage cost by using SpareSpace.
+            </ControlLabel>
+            <FormControl type="text" placeholder="Zip code" />
+          </FormGroup>
+        </Col>
+      </Row>
+    );
+  }
+}
