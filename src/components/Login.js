5ecc7e91b5b8dede20461deb3fa5ab7952b5c023
Unknown <dhanany@zagmail.gonzaga.edu>
2017-10-10 10:41:15 -0700

    Auto stash before merge of "develop" and "origin/develop"

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,105 +11,123 @@
  *
  */
 
-
 import React, { Component } from "react";
-import { Formik } from 'formik'
-import axios from 'axios'
+import { Redirect } from "react-router-dom";
+import { Formik } from "formik";
+import axios from "axios";
 
 class Login extends Component {
-    render() {
+  render() {
+    console.log("inside render", this.context.router);
+    const loginStyle = {
+      marginTop: 100
+      //adjust login header to be more down the screen
+    };
 
-        //adjust login header to be more down the screen
-        const loginStyle = {
-            marginTop: 100
-        }
+    //adjust forms to have more space against login header
+    const formStyle = {
+      marginTop: 25
+    };
 
-        //adjust forms to have more space against login header
-        const formStyle = {
-            marginTop: 25
-        }
+    return (
+      <div>
+        <h1 style={loginStyle} className="text-center">
+          {" "}
+          Login{" "}
+        </h1>
+        <div className="container text-center">
+          <Formik
+            initialValues={{
+              email: "",
+              password: ""
+            }}
+            //formik is handling our forms. It will check for valid
+            //input, and then send info on click "Login" to our backend
 
+            //makes sure valid email is entered
+            validate={values => {
+              let errors = {};
+              if (!values.email) {
+                errors.email = "Required";
+              } else if (!values.password) {
+                errors.password = "Required";
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+              ) {
+                errors.email = "Invalid email address";
+              }
+              return errors;
+            }}
+            onSubmit={values => {
+              console.log(values);
+              axios
+                .post("http://localhost:3001/login", values)
+                .then(function (response) {
+                  console.log(response.data[0]);
 
-        return (
-            <div>
-                <h1 style={loginStyle} className="text-center"> Login </h1>
-                    <div className="container text-center">
-                        <Formik initialValues={{
-                            email: '',
-                            password: ''
-                        }}
-                                //formik is handling our forms. It will check for valid
-                                //input, and then send info on click "Login" to our backend
-
-
-                        //makes sure valid email is entered
-                        validate={values => {
-
-                            let errors = {}
-                            if (!values.email) {
-                                errors.email = 'Required'
-                            }
-                            else if (!values.password){
-                                errors.password = 'Required'
-                            }
-                            else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-                                errors.email = 'Invalid email address'
-                            }
-                            return errors
-                        }}
-
-                         onSubmit={(values) => {
-                                console.log(values)
-                                axios.post('http://localhost:3001/login', values)
-                                .then(function (response) {
-                                    console.log(response.data[0]);
-                                    console.log("You're logged in!")
-                                    var logged = true;
-                                  })
-                                  .catch(function (error) {
-                                    var logged = false;
-                                    console.log('Invalid log in!');
-                                  })
-                         }}
-
-                                //render is actually rendering the form for the user to see
-                        render={({ values, touched, errors, handleChange, handleSubmit, isSubmitting }) =>
-                            <form style={formStyle} onSubmit={handleSubmit}>
-                                <div className="form-group">
-                                    <label className="pull-left">Email address</label>
-                                    <input
-                                        id="email"
-                                        className="form-control"
-                                        type="email"
-                                        name="email"
-                                        placeholder="Email"
-                                        onChange={handleChange}
-                                        value={values.email}
-                                    />
-                                    {touched.email && errors.email && <div>{errors.email}</div>}
-                                </div>
-                                <div className="form-group">
-                                    <label className="pull-left">Password</label>
-                                    <input
-                                        id="password"
-                                        className="form-control"
-                                        type="password"
-                                        name="password"
-                                        placeholder="Password"
-                                        onChange={handleChange}
-                                        value={values.password}
-                                    />
-                                    {touched.password && errors.password && <div>{errors.password}</div>}
-                                </div>
-                                <button className="btn btn-default" type="submit">Login</button>
-                            </form>}
-                        />
-                    </div>
-            </div>
-
-        );
-    }
+                  //console.log(response.data[0].fullname);
+                  console.log("You're logged in!");
+                  //return this.context.router.history.replace("/logged_in");
+                  window.location.href = "/users/" + response.data[0]._id
+                  //return <Redirect to="/logged_in" />;
+                  //var logged = true;
+                })
+                .catch(function (error) {
+                  //var logged = false;
+                  console.log("Invalid log in!");
+                });
+            }}
+            //render is actually rendering the form for the user to see
+            render={({
+              values,
+              touched,
+              errors,
+              handleChange,
+              handleSubmit,
+              isSubmitting
+            }) => (
+                <form style={formStyle} onSubmit={handleSubmit}>
+                  <div className="form-group">
+                    <label className="pull-left">Email address</label>
+                    <input
+                      id="email"
+                      className="form-control"
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      onChange={handleChange}
+                      value={values.email}
+                    />
+                    {touched.email && errors.email && <div>{errors.email}</div>}
+                  </div>
+                  <div className="form-group">
+                    <label className="pull-left">Password</label>
+                    <input
+                      id="password"
+                      className="form-control"
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      onChange={handleChange}
+                      value={values.password}
+                    />
+                    {touched.password &&
+                      errors.password && <div>{errors.password}</div>}
+                  </div>
+                  <button className="btn btn-default" type="submit">
+                    Login
+                </button>
+                </form>
+              )}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
+Login.contextTypes = {
+  router: React.PropTypes.func.isRequired
+};
 
 export default Login;
--- src/components/Nav-bar.js
+++ src/components/Nav-bar.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import { Navbar, Nav, NavItem } from "react-bootstrap";
-import { LinkContainer } from 'react-router-bootstrap';
-import {Route, Redirect} from 'react-router-dom';
+import { LinkContainer } from "react-router-bootstrap";
+import { Route, Redirect } from "react-router-dom";
 import Login from "./Login";
+import LoggedIn from "./LoggedIn";
 import Home from "./Home";
 import SignUp from "./SignUp";
-
-
 
 export default class AppNavbar extends Component {
   render() {
@@ -14,25 +13,32 @@ export default class AppNavbar extends Component {
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>
-              <LinkContainer to="/">
-                  <a href="/home">sparespace</a>
-              </LinkContainer>
+            <LinkContainer to="/">
+              <a href="/home">sparespace</a>
+            </LinkContainer>
           </Navbar.Brand>
         </Navbar.Header>
         <Nav pullRight>
-            {/*here link container acts just like Link from react-router, but for react-router-bootstrap'*/}
-            <LinkContainer to="/login" activeClassName="none">
-                <NavItem eventKey={2}>Login</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/sign_up">
-                <NavItem eventKey={3}>Sign Up</NavItem>
-            </LinkContainer>
+          {/*here link container acts just like Link from react-router, but for react-router-bootstrap'*/}
+          <LinkContainer to="/login" activeClassName="none">
+            <NavItem eventKey={2}>Login</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/sign_up">
+            <NavItem eventKey={3}>Sign Up</NavItem>
+          </LinkContainer>
         </Nav>
-          {/*need to find a better way of storing these routes...*/}
-          <Route exact path="/" render={()=> <Redirect to='/home'/>}/>
-          <Route path="/login" component={Login}> </Route>
-          <Route path="/home" component={Home}> </Route>
-          <Route path="/sign_up" component={SignUp}> </Route>
+        {/*need to find a better way of storing these routes...*/}
+        <Route exact path="/" render={() => <Redirect to="/home" />} />
+        <Route path="/login" component={Login}>
+          {" "}
+        </Route>
+        <Route path="/home" component={Home}>
+          {" "}
+        </Route>
+        <Route path="/users/:id" component={LoggedIn} />
+        <Route path="/sign_up" component={SignUp}>
+          {" "}
+        </Route>
       </Navbar>
     );
   }
