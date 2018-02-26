283d755bccbfefae0f6786447ba4244bbebf738d
Devin Roche <Roche.devin@ymail.com>
2018-02-26 14:20:06 -0800

    closing listings
    prevents other users from finding a closed listing

--- src/components/Profile/DeleteListing.js
+++ src/components/Profile/DeleteListing.js
@@ -19,11 +19,42 @@ class DeleteListing extends Component {
         this.handleSubmit = this.handleSubmit.bind(this)
     }
 
-
+    componentDidMount() {
+        this.renderDelete();
+    }
     handleSubmit(event) {
         axios.delete(`http://localhost:3001/listing/${this.state.listing_id}`)
     }
-
+    renderDelete() {
+        return (
+            swal({
+                title: 'Are you sure?',
+                text: 'Your listing will be removed!',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, delete it!',
+                cancelButtonText: 'No, keep it'
+            }).then((result) => {
+                if (result.value) {
+                    swal(
+                        'Deleted!',
+                        'Your imaginary file has been deleted.',
+                        'success'
+                    ).then(() => {
+                        this.handleSubmit()
+                        window.location.href = `/users/${Cookies.getId()}`
+                        return <Redirect to={"/users/" + Cookies.getId()} />
+                    })
+                } else if (result.dismiss === swal.DismissReason.cancel) {
+                    swal(
+                        'Cancelled',
+                        'Your listing lives to die another day!',
+                        'error'
+                    ).then(() => this.props.toggle)
+                }
+            })
+        )
+    }
 
     render() {
         let alert = (
@@ -40,24 +71,24 @@ class DeleteListing extends Component {
                         'Deleted!',
                         'Your imaginary file has been deleted.',
                         'success'
-                    )
-                    this.handleSubmit()
-                    window.location.href = `/users/${Cookies.getId()}`
-                    return <Redirect to={"/users/" + Cookies.getId()} />
-
+                    ).then(() => {
+                        this.handleSubmit()
+                        window.location.href = `/users/${Cookies.getId()}`
+                        return <Redirect to={"/users/" + Cookies.getId()} />
+                    })
                 } else if (result.dismiss === swal.DismissReason.cancel) {
                     swal(
                         'Cancelled',
                         'Your listing lives to die another day!',
                         'error'
-                    )
-                    this.props.toggle
+                    ).then(() => this.props.toggle)
                 }
             })
         )
+        console.log(alert)
         return (
             <div>
-                {alert}
+                {this.renderDelete}
             </div>
         );
     }
